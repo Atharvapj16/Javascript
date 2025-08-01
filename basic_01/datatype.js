f43e61ff7d4651c0ca5d7ea3a8d7cef54be488ae
Atharva Jadhav <159755582+Atharvapj16@users.noreply.github.com>
2025-08-01 05:36:08 +0000

    object

--- basic_01/datatype.js
+++ basic_01/datatype.js
@@ -21,3 +21,30 @@ because of issue in block scope and functional scope
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 /* Output for  accountState is undefined since we have not initialize any value*/
+
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser, it work only in browser 
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
